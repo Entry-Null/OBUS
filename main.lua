ca170c90abdd7e8d6654966b405ab56b1fe2cd59
Entry-Null <72706329+Entry-Null@users.noreply.github.com>
2020-12-03 13:35:42 -0600

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,20 @@
+
+
+options = {
+    Complex = 14,
+    MaxBranch = 16,
+    Comments = 700,
+    MemeComments = true,
+    MemeConstantChance = 810,
+    ConstantChance = 1000,
+    Input = "Input.txt",
+    Output = "Output.txt"
+}
+
+
+unpack = unpack or table.unpack
+load = loadstring or load
+loadstring = loadstring or load
+
+
+require("constants")
